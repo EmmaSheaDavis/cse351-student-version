9fdf15122dfc37178f4d3cb10ccc529ac8123029
EmmaSheaDavis <emmadavis@byui.edu>
2025-05-30 10:37:10 -0600

    stuff

--- lesson_11/prep/AtomicExample/obj/Debug/net8.0/AtomicExample.AssemblyInfo.cs
+++ lesson_11/prep/AtomicExample/obj/Debug/net8.0/AtomicExample.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AtomicExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90987b0cc181c0141115b2ad995aa84264c4a31b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69eb73508fd0fde3b7f6f057fdd785178ad795b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AtomicExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AtomicExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
